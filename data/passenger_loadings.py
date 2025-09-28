02f0ac77d96f254f8b4194f7ab614ae4380d5dc4
Marta Zarantonello <2555820Z@student.gla.ac.uk>
2025-09-28 20:01:44 +0100

    SWR passenger loadings

--- data/passenger_loadings.py
+++ data/passenger_loadings.py
@@ -1,4 +1,12 @@
 # one company's (SWR) passenger loadings data
 SWR_passenger_loadings_data = {
-    "SWR passenger loadings" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\SWR data\SWR_Passenger_Loadings.json"
+    "24-25 P10" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P10.xlsx"
+    "24-25 P11" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P11.xlsx"
+    "24-25 P12" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P12.xlsx"
+    "24-25 P13" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P13.xlsx"
+    "25-26 P01" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P01.xlsx"
+    "25-26 P02" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P02.xlsx"
+    "25-26 P03" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P03.xlsx"
+    "25-26 P04" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P04.xlsx"
+
 }
