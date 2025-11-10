7c873a3940ca82d36e14503d1df426b57617fd7e
Marta Zarantonello <2555820Z@student.gla.ac.uk>
2025-11-10 16:19:25 +0000

    defined its connection to SWR data

--- data/incidents.py
+++ data/incidents.py
@@ -1,16 +1,21 @@
-# period: yearly
-incident_files = {
-    "23-24 P12" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202324\Transparency 23-24 P12.csv",
-    "23-24 P13" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202324\Transparency 23-24 P13.csv",
-    "24-25 P01" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P01.csv",
-    "24-25 P02" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P02.csv",
-    "24-25 P03" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P03.csv",
-    "24-25 P04" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P04.csv",
-    "24-25 P05" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P05.csv",
-    "24-25 P06" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P06.csv",
-    "24-25 P07" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P07.csv",
-    "24-25 P08" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P08.csv",
-    "24-25 P09" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P09.csv",
-    "24-25 P10" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\incident data\202425\Transparency 24-25 P10.csv",
+# period: one financial/rail year
 
+from pathlib import Path
+
+# Base data directory (relative to repo root)
+DATA_DIR = Path(__file__).resolve().parents[1] / "data"
+
+incident_files = {
+    "23-24 P12": DATA_DIR / "Transparency_23-24_P12.csv",
+    "23-24 P13": DATA_DIR / "Transparency_23-24_P13.csv",
+    "24-25 P01": DATA_DIR / "Transparency_24-25_P01.csv",
+    "24-25 P02": DATA_DIR / "Transparency_24-25_P02.csv",
+    "24-25 P03": DATA_DIR / "Transparency_24-25_P03.csv",
+    "24-25 P04": DATA_DIR / "Transparency_24-25_P04.csv",
+    "24-25 P05": DATA_DIR / "Transparency_24-25_P05.csv",
+    "24-25 P06": DATA_DIR / "Transparency_24-25_P06.csv",
+    "24-25 P07": DATA_DIR / "Transparency_24-25_P07.csv",
+    "24-25 P08": DATA_DIR / "Transparency_24-25_P08.csv",
+    "24-25 P09": DATA_DIR / "Transparency_24-25_P09.csv",
+    "24-25 P10": DATA_DIR / "Transparency_24-25_P10.csv", # only data set combined with passenger loadings
 }
