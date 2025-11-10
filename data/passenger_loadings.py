8cc5798548f787018bf51fbbc2b44d36bf24e773
Marta Zarantonello <2555820Z@student.gla.ac.uk>
2025-11-10 16:18:45 +0000

    passenger loadings updated

--- data/passenger_loadings.py
+++ data/passenger_loadings.py
@@ -1,12 +1,18 @@
 # one company's (SWR) passenger loadings data
-SWR_passenger_loadings_data = {
-    "24-25 P10" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P10.xlsx"
-    "24-25 P11" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P11.xlsx"
-    "24-25 P12" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P12.xlsx"
-    "24-25 P13" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY25 P13.xlsx"
-    "25-26 P01" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P01.xlsx"
-    "25-26 P02" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P02.xlsx"
-    "25-26 P03" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P03.xlsx"
-    "25-26 P04" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\c:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\SWR data\SWR Passenger Loadings - RY26 P04.xlsx"
+# NB: only RY25 P10 is used in the analysis as the delay data only covers until month. The P11 to P13 delay data is not yet available.
 
+from pathlib import Path
+
+# Base data directory (relative to repo root)
+DATA_DIR = Path(__file__).resolve().parents[1] / "data"
+
+SWR_passenger_loadings_data_files = {
+    "24-25 P10" : DATA_DIR / "SWR Passenger Loadings - RY25 P10.xlsx", # only this month is used in this analysis
+    "24-25 P11" : DATA_DIR / "SWR Passenger Loadings - RY25 P11.xlsx",
+    "24-25 P12" : DATA_DIR / "SWR Passenger Loadings - RY25 P12.xlsx",
+    "24-25 P13" : DATA_DIR / "SWR Passenger Loadings - RY25 P13.xlsx",
+    "25-26 P01" : DATA_DIR / "SWR Passenger Loadings - RY26 P01.xlsx",
+    "25-26 P02" : DATA_DIR / "SWR Passenger Loadings - RY26 P02.xlsx",
+    "25-26 P03" : DATA_DIR / "SWR Passenger Loadings - RY26 P03.xlsx",
+    "25-26 P04" : DATA_DIR / "SWR Passenger Loadings - RY26 P04.xlsx",
 }
