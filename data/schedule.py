dab5f53a2c7a1912924c4a5de0e257ef27682a08
Marta Zarantonello <2555820Z@student.gla.ac.uk>
2025-11-10 16:20:20 +0000

    schedule cleaning added

--- data/schedule.py
+++ data/schedule.py
@@ -1,3 +1,11 @@
+# IMPORTANT: please run schedule_cleaning.py first to generate the cleaned schedule pickle file.
+
+# Base data directory (relative to repo root)
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parents[1] / "data"
+
 schedule_data = {
-    "toc full" : r"C:\Users\39342\University of Glasgow\Ji-Eun Byun - MZ-JB\MSci (Research) 2024-25\schedule data\toc-full_split_part4.pkl"
+   "toc full": DATA_DIR / "CIF_ALL_FULL_DAILY_toc-full.json.gz", # file directly downloaded from RDM, contains 5 parts (4th part is the schedule)
+   "schedule" : DATA_DIR / "CIF_ALL_FULL_DAILY_toc-full_p4.pkl", # cleaned schedule file in pickle format (only 4th part - actual schedule)
 }
