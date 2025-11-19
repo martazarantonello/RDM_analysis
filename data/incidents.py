9fb6c878b0a6be4778a32226b0b9f6729f817602
Marta Zarantonello <2555820Z@student.gla.ac.uk>
2025-11-19 11:36:39 +0000

    minor change in names

--- data/incidents.py
+++ data/incidents.py
@@ -11,16 +11,16 @@ from pathlib import Path
 DATA_DIR = Path(__file__).resolve().parents[1] / "data"
 
 incident_files = {
-    "23-24 P12": DATA_DIR / "Transparency_23-24_P12.csv",
-    "23-24 P13": DATA_DIR / "Transparency_23-24_P13.csv",
-    "24-25 P01": DATA_DIR / "Transparency_24-25_P01.csv",
-    "24-25 P02": DATA_DIR / "Transparency_24-25_P02.csv",
-    "24-25 P03": DATA_DIR / "Transparency_24-25_P03.csv",
-    "24-25 P04": DATA_DIR / "Transparency_24-25_P04.csv",
-    "24-25 P05": DATA_DIR / "Transparency_24-25_P05.csv",
-    "24-25 P06": DATA_DIR / "Transparency_24-25_P06.csv",
-    "24-25 P07": DATA_DIR / "Transparency_24-25_P07.csv",
-    "24-25 P08": DATA_DIR / "Transparency_24-25_P08.csv",
-    "24-25 P09": DATA_DIR / "Transparency_24-25_P09.csv",
-    "24-25 P10": DATA_DIR / "Transparency_24-25_P10.csv", # only data set combined with passenger loadings
+    "23-24 P12": DATA_DIR / "Transparency 23-24 P12.csv",
+    "23-24 P13": DATA_DIR / "Transparency 23-24 P13.csv",
+    "24-25 P01": DATA_DIR / "Transparency 24-25 P01.csv",
+    "24-25 P02": DATA_DIR / "Transparency 24-25 P02.csv",
+    "24-25 P03": DATA_DIR / "Transparency 24-25 P03.csv",
+    "24-25 P04": DATA_DIR / "Transparency 24-25 P04.csv",
+    "24-25 P05": DATA_DIR / "Transparency 24-25 P05.csv",
+    "24-25 P06": DATA_DIR / "Transparency 24-25 P06.csv",
+    "24-25 P07": DATA_DIR / "Transparency 24-25 P07.csv",
+    "24-25 P08": DATA_DIR / "Transparency 24-25 P08.csv",
+    "24-25 P09": DATA_DIR / "Transparency 24-25 P09.csv",
+    "24-25 P10": DATA_DIR / "Transparency 24-25 P10.csv", # only data set combined with passenger loadings
 }
